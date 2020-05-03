f99eff4e37f9cd66faedf0db48a6371b58e8728f
Pinky Gautam <57994731+ppkgtmm@users.noreply.github.com>
2020-05-03 23:20:06 +0700

    day 19

--- 19 - Autocomplete/start.js
+++ 19 - Autocomplete/start.js
@@ -1,3 +1,59 @@
 (() => {
-  // เริ่มเขียนโค้ด
+
+const carBrands = [
+  'BMW',
+  'Maserati',
+  'Mercedes Benz',
+  'Ferrari',
+  'Toyata',
+  'Honda',
+  'Hyundai'
+];
+
+const searchElem = document.querySelector('.search');
+
+function selectCarBrand(event)
+{
+  searchElem.value = event.target.innerText;
+  clearResults();
+}
+
+function clearResults() {
+  const results = document.querySelector('.results');
+  if(results)
+  {
+    document.body.removeChild(results);
+  }
+}
+
+function createUlElement() {
+  const ulElem = document.createElement('ul');
+  ulElem.classList.add('results');
+  return ulElem;
+}
+
+function onInput(event) {
+  clearResults();
+  const input = event.target.value.toLowerCase();
+  const matchedItems = carBrands.filter(car => car.toLowerCase().startsWith(input));
+  const ulElem = createUlElement();
+  matchedItems.forEach(car => {
+    const liElem = document.createElement('li');
+    liElem.innerText = car
+    liElem.onclick = selectCarBrand;
+    ulElem.appendChild(liElem);
+  });
+  if (matchedItems!==undefined && matchedItems.length>0)
+  {
+       document.body.appendChild(ulElem);
+  }
+
+
+
+}
+function run() {
+searchElem.addEventListener('input',onInput);
+document.addEventListener('click',clearResults)
+}
+run();
 })();

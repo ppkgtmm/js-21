822890779d9f226694d9ab209618cac7896b5809
Pinky Gautam <57994731+ppkgtmm@users.noreply.github.com>
2020-06-13 12:44:09 +0700

    day 21

--- 21 - Infinite Scroll Gallery/start.js
+++ 21 - Infinite Scroll Gallery/start.js
@@ -1,3 +1,45 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  const key = 'XQl5G1YE5nmUuUHzfKM5r1Q3albqEqKQH4SILpPc5Yg';
+  const galleryElem = document.querySelector('.gallery');
+  const loaderElem = document.querySelector('.loader');
+  let page = 1;
+
+  function showLoader() {
+    loaderElem.classList.add('visible');
+  }
+
+  function hideLoader() {
+    loaderElem.classList.remove('visible');
+  }
+
+
+  function createImageElem(image) {
+    const imageElem = document.createElement('img');
+    imageElem.src = image.urls.small;
+    galleryElem.appendChild(imageElem);  
+  }
+
+  async function displayImages() {
+  showLoader();
+  const result = await fetch(`https://api.unsplash.com/photos?client_id=${key}&page=${page}`);
+  const images = await result.json();
+  // console.log(images);
+  images.forEach(image => {
+    createImageElem(image);    
+  });
+  page++;
+  hideLoader();
+  }
+
+function onScroll() {
+  const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
+  if(scrollTop+clientHeight>=scrollHeight-10){    displayImages();
+  }
+}
+
+  function run() {
+   displayImages();
+   document.addEventListener('scroll',onScroll);
+  }
+  run();
 })();
